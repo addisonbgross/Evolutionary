522d221d109882dae92fa09d3532b2de28d1b2ae
maxlazarus <max.prokopenko@gmail.com>
2014-11-06 16:55:50 -0800

    Selector started

--- Source/Selector.cs
+++ Source/Selector.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Evolutionary
-{
-	public class Selector
-	{
-		public Selector ()
-		{
+namespace Selector {
+	public class Selector {
+		static Dictionary<int, String> deathList = new Dictionary<float, String> ();
+		static Random random = new Random();
+		static float killLevel;
+
+		public Selector (){}
+
+		public void Select (Dictionary<string, float> chromosomes, float killRate) {
+
+			deathList.Clear;
+			killLevel = 0;
+
+			var keys = new List<string> (chromosomes.Keys);
+		
+			foreach (string key in keys) {
+				deathList.Add (random.Next, key);
+				killLevel += chromosomes [key];
+				//chromosomes [key] = Math.Round (chromosomes[key], 3);
+			}
+
+			killLevel *= killRate;
+			List<KeyValuePair<int, String>> list = deathList;
+			list.Sort ();
+
+			foreach (String s in list) {
+				if (killLevel -= chromosomes (s) < 0) {
+					break;
+				}
+				chromosomes.Remove (s);
+			}
 		}
 	}
 }
---
+++ Tests/SelectorTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Test
+{
+	[TestFixture()]
+	public class SelectorTests
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
