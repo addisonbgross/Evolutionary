6de04b826dccc9ed3a8754d1149ec19d837b5ef4
maxlazarus <max.prokopenko@gmail.com>
2014-11-13 15:30:04 -0800

    fixed sorting

--- Source/Selector.cs
+++ Source/Selector.cs
@@ -1,6 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-
+/**
+ * The basic idea:
+ * -get a set of chromosomes
+ * -iterate through the chromosomes
+ * 		-add each fitness value to killLevel
+ * 		-add the chromosome and a random number to deathList
+ * -multiply killLevel by the percentage of chromosomes to be killed
+ * -sort deathList randomly
+ * -iterate through deathList
+ * 		-subtract the chromosome's fitness from the killLevel
+ * 		-if the killLevel is below zero break
+ * 		-remove the current chromosome from the original set
+ */
 namespace Selector {
 	public class Selector {
 		static List<KeyValuePair<int, String>> deathList = new List<KeyValuePair<int, String>> ();
@@ -14,6 +26,11 @@ namespace Selector {
 			Selector s = new Selector ();
 			Dictionary<string, float> d = new Dictionary<string, float>();
 			d.Add ("SADFASD", 1.0f);
+			d.Add ("SADTTSD", 0.9f);
+			d.Add ("SASD", 0.8f);
+			d.Add ("SADTSD", 0.1f);
+			d.Add ("SAFFSD", 0.1f);
+			d.Add ("SASSDTSD", 0.1f);
 			s.Select (d, 1.0f);
 			Console.WriteLine (d.Count);
 		}
@@ -31,11 +48,10 @@ namespace Selector {
 				deathList.Add (kvp);
 
 				killLevel += chromosomes [key];
-				//chromosomes [key] = Math.Round (chromosomes[key], 3);
 			}
 
 			killLevel *= killRate;
-			deathList.Sort ();
+			deathList.Sort (CustomSort);
 
 			foreach (KeyValuePair<int, String> kvp in deathList) {
 				if ((killLevel -= chromosomes[kvp.Value]) < 0) {
@@ -43,6 +59,15 @@ namespace Selector {
 				}
 				chromosomes.Remove (kvp.Value);
 			}
+		}
+
+		public int CustomSort(KeyValuePair<int, string> x, KeyValuePair<int, String> y) {
+			if (x.Key > y.Key)
+				return 1;
+			else if (y.Key < x.Key)
+				return -1;
+			else
+				return 0;
 		}
 	}
 }
