c48ad9de72f1cb32c7b30b2ce953ea8020841ded
maxlazarus <max.prokopenko@gmail.com>
2014-12-01 23:07:41 -0800

    Working on Mutator

--- Source/Evolutionary.cs
+++ Source/Evolutionary.cs
@@ -14,6 +14,7 @@ namespace Evolutionary
 			evo.SetSizeRequest (600,300);
 			evo.DeleteEvent += OnDeleteEvent;
 
+			Mutator m = new Mutator ();
 			Generator g = new Generator ();
 			GraphView v = new GraphView ();
 			Controller c = new Controller ();
@@ -24,8 +25,14 @@ namespace Evolutionary
 			Dictionary<string, float> chromosomes = new Dictionary<string, float> ();
 			for (int i = 0; i < 10; i++)
 				chromosomes.Add (g.generateChromosome(), (float)i/10);
+
+			PrintChromosomes (chromosomes);
+			chromosomes = m.Mutate (chromosomes, 0.1f, 0.3f);
+			PrintChromosomes (chromosomes);
+
 			chromosomes = d.decode (5, chromosomes); //NOT WORKING
 			v.SetValues (chromosomes);
+
 
 			tBox.Add (c.GetInterface ());
 			tBox.Add (v.GetView());
@@ -34,9 +41,12 @@ namespace Evolutionary
 			evo.ShowAll ();
 			Application.Run ();
 		}
-		static void OnDeleteEvent (object obj, EventArgs args)
-		{
+		static void OnDeleteEvent (object obj, EventArgs args) {
 			Application.Quit ();
+		}
+		public static void PrintChromosomes(Dictionary<string, float> d) {
+			foreach (KeyValuePair<string, float> kvp in d)
+				Console.WriteLine(kvp.Key);
 		}
 	}
 }
